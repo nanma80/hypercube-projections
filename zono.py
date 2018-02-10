907121f267ef7546e0e9faaa436bb3f00f416b3f
Nan Ma <nanma@microsoft.com>
2018-02-09 17:55:52 -0800

    simple improvement

--- zono.py
+++ zono.py
@@ -4,18 +4,27 @@ import polytope_builder as pb
 
 dimension = 4
 
-# all_vertices = pb.get_600_cell_vertices()
+# all_vertices = pb.get_5_cell_vertices()
 all_vertices = pb.get_orthoplex_vertices(dimension)
+# all_vertices = pb.get_cube_vertices(dimension)
+# all_vertices = pb.get_24_cell_vertices()
+# all_vertices = pb.get_600_cell_vertices()
 
-points = [ pb.get_origin(dimension) ]
+
+# todo: randomize the order in all_vertices
+# dedup
+
+points = [ np.array(pb.get_origin(dimension)) ]
 
 for v in all_vertices:
 	points = points[:]
 	new_points = [[sum(x) for x in zip(p, v)] for p in points]
 	points = np.concatenate((points, new_points), axis = 0)
 	if len(points) > 100:
-		points = ConvexHull(points).points
+		ch = ConvexHull(points)
+		ch_vertices = ch.vertices
+		points = [points[index] for index in ch.vertices]
 
 print len(points)
-for p in points:
-	print p
+# for p in points:
+# 	print p
