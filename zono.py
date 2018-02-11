68232741002d415f0399f441d1b60064eca12ac7
Nan Ma <nanma@microsoft.com>
2018-02-11 11:42:16 -0800

    investigting zono(tesseract)

--- zono.py
+++ zono.py
@@ -12,8 +12,8 @@ dimension = 4
 
 # all_vertices = pb.get_5_cell_vertices()
 # all_vertices = pb.get_orthoplex_vertices(dimension)
-# all_vertices = pb.get_cube_vertices(dimension)
-all_vertices = pb.get_24_cell_vertices()
+all_vertices = pb.get_cube_vertices(dimension)
+# all_vertices = pb.get_24_cell_vertices()
 # all_vertices = pb.get_600_cell_vertices()
 # all_vertices = pb.get_120_cell_vertices()
 
@@ -38,19 +38,28 @@ for v in all_vertices:
 
 points = convex_hull(points)
 
-scaling = sp.sqrt(1) * 2
+scaling = sp.sqrt(1) * 1
 points = [p/scaling for p in points]
 points.sort(key=lambda x:x[-1] * 1000000 + x[-2] * 10000 + x[-3] * 100 + x[-4])
 
 print "vertex count:", len(points)
 print "vertices:"
-for p in points:
-	print p
+# for p in points:
+# 	print p
 
+# investigating zono(tesseract)
+# selected_points = [p for p in points if 
+# 	1==1
+# 	# and (p[2] + p[3] == 4) 
+# 	and (p[0] + p[1] + p[2] + p[3] == 6)
+# 	]
+# print len(selected_points)
+# for p in selected_points:
+# 	print p
 
 # 5-cell -> 30
 # 16-cell -> 16 (hypercube)
-# 8-cell -> 104
+# 8-cell -> 104, 40-cell: 24 dihedral rhombic dodecahedron + 16 cubes
 # 24-cell -> 192
 # 600-cell -> 7616 (have to use math.sqrt, otherwise too slow)
 # 120-cell didn't try
