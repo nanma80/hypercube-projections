03a87d967b1b86438fffa674f0c2f6b08adb24ae
Nan Ma <nanma@microsoft.com>
2023-04-18 17:07:36 -0700

    analyze cells

--- max_shadow_8_4_v3.py
+++ max_shadow_8_4_v3.py
@@ -13,6 +13,7 @@ from scipy.spatial import ConvexHull
 
 high_dimension = 8
 low_dimension = 4
+epsilon = 0.0000001;
 
 class Bounds(object):
   def __init__(self, xmax=[1,1], xmin=[0,0] ):
@@ -71,7 +72,7 @@ def negative_volume(generators):
 def maximize_shadow():
   # generators = [0.378556, 0.326643, 0.100944, 0.489704 ]
   # generators = [0.711917, 0.203285]
-  generators = random.rand(2,1)
+  generators = random.rand(2,1) * 0.08
   # generators = [math.pi/4, math.pi/4]
   # print "Initial generator is:"
   # print generators
@@ -101,11 +102,11 @@ def convex_hull(vertices, bases):
   normalizing_factor = math.sqrt(norm)
   # phi = (1 + sqrt(5))/2
   # normalizing_factor = phi
-  normalized_bases = bases.T #/ normalizing_factor
+  normalized_bases = bases.T / normalizing_factor
 
   v2d = np.dot(vertices, normalized_bases)
   hull = ConvexHull(v2d)
-  return hull
+  return hull, v2d
 
 def get_cube_vertices(dimension):
   vertices = []
@@ -125,15 +126,79 @@ def main():
   # print orth_optimal_bases
 
   vertices = array(get_cube_vertices(8))
-  hull = convex_hull(vertices, orth_optimal_bases)
+  hull, projected_vertices = convex_hull(vertices, orth_optimal_bases)
+  print "convex hull volume: ", hull.volume
   hull_vertices = [hull.points[vertex_index] for vertex_index in hull.vertices]
   hull_vertices.sort(key=lambda x:x[3])
 
   print "# vertices in the convex hull:", len(hull.vertices)
-  for index, vertex in enumerate(hull_vertices):
-    print ''.join(["%02d" % (index+1), ': ['] + ["{:10.4f}".format(x) for x in vertex] + [']'])
+  # for index, vertex in enumerate(hull_vertices):
+    # print ''.join(["%02d" % (index+1), ': ['] + ["{:10.4f}".format(x) for x in vertex] + [']'])
   # print hull.vertices
+
+  # before dedup, > 800 equations
+  # print hull.equations
+  # print len(hull.equations)
+
+  rounded_equations = array([[np.round(x, 15) for x in v] for v in hull.equations])
+  # rounded_equations = [tuple(row) for row in rounded_equations]
+  unique_equations = np.unique(rounded_equations, axis=0)
+
+  # print unique_equations
+  print len(unique_equations)
+
+  offsets = [np.round(v[4], 6) for v in unique_equations]
+  print sorted(np.unique(offsets))
+
+  # after dedup, 112 cells, 128 vertices. Each cell has 8 vertices and is an affine cross polytope. Each vertex belong to 7 cells? no. Some vertex belong to more. some fewer.
+
+  ids_in_cells = []
+
+  for equation in unique_equations:
+    ids_in_cell = [vid for vid in range(len(projected_vertices)) if dot( np.concatenate((projected_vertices[vid], array([1]))) , equation ) > - epsilon]
+    ids_in_cells.append(ids_in_cell)
+    # print ids_in_cell
+
+  # print ids_in_cells
+  # each cell has 8 vertices. is an affine cross polytope
+  cell_centers = []
+  for ids in ids_in_cells:
+    projected_vertices_in_cell = [projected_vertices[id] for id in ids]
+    center = array(np.average(projected_vertices_in_cell, axis=0))
+    relative_vertices = array([ np.round(v - center, 10) for v in projected_vertices_in_cell])
+    negative_relative_vertices = array([ np.round(- v + center, 10) for v in projected_vertices_in_cell])
+    all_relative_vertices = np.concatenate((relative_vertices, negative_relative_vertices))
+    # print(len(all_relative_vertices))
+    # print( len(np.unique(all_relative_vertices, axis=0)) )
+    # all 8. all cells are cross polytopes
+    # print( np.unique(all_relative_vertices, axis=0) )
+
+    # print(center)
+    # print(array([ v - center for v in projected_vertices_in_cell] ))
+
+    # print(array([ np.linalg.norm(v- center) for v in projected_vertices_in_cell] ))
+
+    # break
+
+  # for projected_vertex in projected_vertices:
+  #   equations_on_vertex = [eid for eid in range(len(unique_equations)) if dot( np.concatenate( (array(projected_vertex), array([1])) ), unique_equations[eid] ) > - epsilon]
+  #   print(len(equations_on_vertex))
+    # break
+  # print gap.max()
+
+
+  return
+
   
+
+  # print hull.simplices
+  # print len(hull.simplices)
+
+  gap = array([dot( np.concatenate((projected_vertex, array([1]))) , hull.equations[100]) for projected_vertex in projected_vertices])
+  # print gap.max()
+
+  
+
 if __name__ == '__main__':
   main()
 
